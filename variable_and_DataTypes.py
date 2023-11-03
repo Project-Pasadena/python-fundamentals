d2752a2cc00e198262a79046714ebdee1d33ac2a
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2023-11-04 04:11:34 +0600

    trying out varibles in python

---
+++ variable_and_DataTypes.py
@@ -0,0 +1,24 @@
+x = 5 # integer
+y=6 # integer
+z = 7 # integer
+
+
+print(x+y+z) # printing and sum
+
+
+name = 'aLice' #string
+
+print(name); #printing string
+
+address = 'Pasadena , LA , California' #string
+
+print(address) # printing the string 
+
+
+
+is_python_fun = True;
+
+print(is_python_fun);
+
+senior_developer = False;
+print(senior_developer);
