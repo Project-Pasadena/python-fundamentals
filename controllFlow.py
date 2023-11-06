2656df4f9fea30fd0df1f435d4b298c36c404454
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2023-11-06 08:31:41 +0600

    c flow

--- controllFlow.py
+++ controllFlow.py
@@ -4,3 +4,12 @@ if age >=18:
     print('You can have a NID')
 else :
     print('Wait for your time')
+    
+    
+    
+    
+number  = 90
+
+if number > 67:
+    print(True)
+else : print(False);
