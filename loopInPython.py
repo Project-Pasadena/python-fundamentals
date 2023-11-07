df11869ce7a9ac2b0cb093d93e1cb86a9dddbed7
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2023-11-08 03:38:35 +0600

    inputOutput.py solvd

--- loopInPython.py
+++ loopInPython.py
@@ -1,2 +1,9 @@
-name = 'sakib'
-print(name)
+# Example of a for loop
+for i in range(5):
+    print(i)
+
+# Example of a while loop
+# count = 0
+# while count < 5:
+#     print(count)
+#     count += 1
