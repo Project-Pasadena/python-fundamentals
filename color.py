6e237f56a837143f2db11daa745e61bc4952168c
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2023-11-08 05:03:20 +0600

    name and color basic is done

---
+++ color.py
@@ -0,0 +1,11 @@
+# asking 2 question about a person 
+    # 1 . His name 
+    # 2. His favourite color
+    
+# than send the message
+
+
+name = input('What is your name')
+color = input('what is your favourite color')
+
+print('hello '+name+'Your favourite color is' + color)
