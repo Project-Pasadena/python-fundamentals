b3b72d4c38a635855404c45bfe92847fa4e33e7e
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2023-11-08 04:37:23 +0600

    variable check out

--- variable_and_DataTypes.py
+++ variable_and_DataTypes.py
@@ -22,3 +22,53 @@ print(is_python_fun)
 
 senior_developer = False # bool.
 print(senior_developer)
+
+
+#integer variables
+
+price = 10
+
+print(price)
+
+
+# define Variables 
+
+
+price = 10
+
+
+print(price)
+
+
+# updating value of python
+
+price = 30
+
+print(price)
+
+
+# floating points 
+
+rating = 4.5
+
+print(rating)
+
+#string 
+
+name = 'Mosh'
+print(name)
+
+
+name = 'sakib'
+print(name)
+
+
+# boolean 
+
+
+is_published = False
+
+print(is_published)
+
+
+  
