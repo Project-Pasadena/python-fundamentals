09ccc4fd93114f266f8307fbe1c4ac7783330960
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2023-11-08 04:51:45 +0600

    basic bad code here

---
+++ hospital.py
@@ -0,0 +1,28 @@
+# imagine we are writing a programme for Hospital in python and we need to check his name , if he is a existing patient , age ,  patient history and assign him to a doctor baseed on his need
+
+
+# taking his name 
+name  = 'Jhon Smith'
+# If he is a existing patient
+if_existing = False
+
+# age ( int )
+
+age = 20
+
+
+# new patiend?
+
+new_Patient = True
+
+# patient History
+
+
+patient_History ='Fever , cough Probably Flu'
+
+# assigned Doctor
+
+doctor = 'Dr. Tahia Rahman'
+
+
+# patient_conclusion = doctor + 'diagnosed that ' + name + age + 'has '+patient_History + 'its Flu' + 'his age is '+age+'and he is a '+new_Patient;
