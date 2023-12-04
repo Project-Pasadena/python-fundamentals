eddacd66e11c14fabc591d0f07a7260d37ba831c
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2023-12-05 01:50:12 +0600

    type casting going on

---
+++ The_GreatShow_of_Python/type_Casting.py
@@ -0,0 +1,17 @@
+# Type Casting in Python : 
+
+# int == string 
+
+age = 21
+
+age +=1
+# print(type(age))
+
+# showing the variable and type casting in string
+
+print("Your age is : "+str(age))
+
+
+height = 250.5
+print(height)
+
