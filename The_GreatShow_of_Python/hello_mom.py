34afaef36b87f93cadbe63e0058a7ec03bda4788
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2023-12-05 01:00:31 +0600

    practicing string variable

--- The_GreatShow_of_Python/hello_mom.py
+++ The_GreatShow_of_Python/hello_mom.py
@@ -1,3 +1,5 @@
 print("Hello mom")
 
 print("My mom is the best person in the world")
+print("She is really angel heart person")
+
---
+++ The_GreatShow_of_Python/variableOfPython.py
@@ -0,0 +1,22 @@
+# Variable : A container for a value . Behaves as the value that in stores 
+
+name = "Sakib"
+# print("Hello "+ name)
+
+# checking the data type of the variable
+
+# print(type(name))
+
+
+# first = "Omar"
+# second = "Ahammed"
+# last = "Sakib"
+
+
+# print(first + second + last)
+
+
+age = 21 
+print(age)
+
+#make sure to use appropriate data type. 
