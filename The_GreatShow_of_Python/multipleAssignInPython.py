f6a8c4bc82f09eac8473d4d7212a8dc8c8588d44
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2023-12-05 21:18:44 +0600

    multiple assigning in python

---
+++ The_GreatShow_of_Python/multipleAssignInPython.py
@@ -0,0 +1,14 @@
+# name = "Makarov"
+
+# age = 21
+
+# attractive = False
+
+# print(name)
+# print(age)
+# print(attractive)
+
+
+name , age , attract = "Sadia" , 22,"Angel"
+
+print(name,age,attract)
