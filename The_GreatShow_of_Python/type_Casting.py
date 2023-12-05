86a77e7ea7aa1e29a41de32034ea1b71544eec77
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2023-12-05 20:40:51 +0600

    variable of Python

--- The_GreatShow_of_Python/type_Casting.py
+++ The_GreatShow_of_Python/type_Casting.py
@@ -15,3 +15,11 @@ print("Your age is : "+str(age))
 height = 250.5
 print(height)
 
+
+# type casting in Boolean
+
+speech = False
+
+story = True
+
+print(" Well the speech was "+str(speech)+" but the story was "+str(story))
