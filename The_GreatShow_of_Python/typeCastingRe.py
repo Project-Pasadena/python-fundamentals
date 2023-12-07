f15b7f53733833a9fa2e97aa56a489cd689670e0
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2023-12-08 03:32:41 +0600

    type casting

---
+++ The_GreatShow_of_Python/typeCastingRe.py
@@ -0,0 +1,15 @@
+# type casting = convert the data type of a value to another data type
+x = 1 # int
+
+y = 2.0 #float
+
+z = "3" #str 
+
+
+print(type(x))
+print(int(y))
+print(type(int(z)))
+
+
+
+# type casting y and Z
