5964648c5ef57319c08e051dfd5be3375c7cf738
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2023-12-08 02:02:24 +0600

    string mathod

--- The_GreatShow_of_Python/stringMathodInPython.py
+++ The_GreatShow_of_Python/stringMathodInPython.py
@@ -21,3 +21,16 @@ print(name.lower())
 # print in upper or to upper
 
 print(name.upper())
+
+
+# print to check if the value of variable is a number or digit
+
+print(name.isdigit()) # it will show a digit
+
+
+print(name.isalpha()) # is all contain a alphabet 
+
+print(name.count("o")) # count how many in the string
+
+print(name.replace("o","a"))
+print(name*3)
