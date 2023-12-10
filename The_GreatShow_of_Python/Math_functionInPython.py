9450fb0c18954193567f8f7fa91bb8f57b57b1e6
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2023-12-11 03:13:25 +0600

    math option in python

--- The_GreatShow_of_Python/Math_functionInPython.py
+++ The_GreatShow_of_Python/Math_functionInPython.py
@@ -6,3 +6,22 @@ print(round(pi)) # rounding a variable
 
 print(math.ceil(pi))
 
+# flooring
+print(math.floor(pi))
+
+
+# absolute value of pie
+
+
+print(abs(pi))
+
+
+# power
+
+print(pow(pi,2))
+
+print(math.sqrt(pi))
+
+# Max of something 
+
+
