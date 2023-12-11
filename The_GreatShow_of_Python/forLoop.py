0e1f445e95dde68ec34e23b4adced9c6d4377872
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2023-12-12 00:20:09 +0600

    for loop details

--- The_GreatShow_of_Python/forLoop.py
+++ The_GreatShow_of_Python/forLoop.py
@@ -1,5 +1,24 @@
+import time
+
 # for loop =  a statement that will execute its block of code a limited amount of times. 
 
 
 for i in range(2,10+1,2):
     print(i)
+
+
+
+name = "Bro Code"
+
+for i in name:
+    print(i)
+
+
+for seconds in range(10,0,-1):
+    print(seconds)
+    time.sleep(1)
+print("Happy New Year")
+
+# While loop  = unlimited 
+
+# For loop  = Limited
