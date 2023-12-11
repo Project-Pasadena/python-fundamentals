0fa600c921646a43fdbc5f71a3559a6166355f5a
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2023-12-11 13:12:51 +0600

    string Slicing

---
+++ The_GreatShow_of_Python/slicing_throughPython.py
@@ -0,0 +1,37 @@
+# slicing  = create a substring by extracting elements from another string indexing[] or slicing()
+
+
+# [start : stop : stop]
+
+# indexing 
+
+
+
+name = "Bro Code"
+
+# just want to have everything 
+
+print(name)
+
+full_name = name[0:] # first to end
+
+first_name = name[:3] # first to third
+
+funky_Name = name[::3] # skipping word
+
+reversed_Word = name[::-1] # [0:end:-1]
+
+# print the first letter 
+first_word = name[0] # first word only
+print(first_name)
+
+print(full_name)
+
+print(first_word)
+
+print(funky_Name)
+
+print(reversed_Word)
+
+# reversing word
+
