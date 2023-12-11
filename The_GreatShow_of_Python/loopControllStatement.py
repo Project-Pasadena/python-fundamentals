51a0eb650c5c89e79324666e3856c1795be15b7d
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2023-12-12 04:26:29 +0600

    loop controll

---
+++ The_GreatShow_of_Python/loopControllStatement.py
@@ -0,0 +1,24 @@
+# loop controll statement = change a loops execution 
+
+# pass does nothing but act as a place holder
+
+# while True:
+#     name = input("Enter your name : ")
+#     if name != "":
+#         break
+
+
+phone_number = "123-451"
+
+# for i in phone_number:
+#     if i =="-":
+#         continue 
+#     print(i, end="")
+    
+    
+
+for i in range(1,21) : 
+    if i==13:
+        pass
+    else:
+        print(i)
