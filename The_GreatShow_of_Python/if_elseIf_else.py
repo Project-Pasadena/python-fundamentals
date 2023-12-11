65cf90da9b6cf6131b0b405a27a529144073ba4e
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2023-12-11 14:27:31 +0600

    if else in python

---
+++ The_GreatShow_of_Python/if_elseIf_else.py
@@ -0,0 +1,13 @@
+age = int(input("How old are you?"))
+
+if age>=18:
+    print("You are an adult")
+
+elif age<0:
+    print("You havent been born")
+# comparison operator
+
+elif age ==100:
+    print("You are about to die")
+else:
+    print("You are a child")
