6d3be2f438312099738f1e75c94806657690e5d6
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2023-12-11 14:41:23 +0600

    logical operator

--- The_GreatShow_of_Python/temoperature.py
+++ The_GreatShow_of_Python/temoperature.py
@@ -2,10 +2,10 @@
 
 temp = int(input("What is the temperature outside : ?"))
 
-if temp>0 and temp<=30 : 
+if not(temp>0 and temp<=30) : 
     print("The Weather is great and good today ")
     print("Go outside")
-elif temp<0 or temp>30:
+elif not(temp<0 or temp>30):
     print("Dont go outside")
 
 
