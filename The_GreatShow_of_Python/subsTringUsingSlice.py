8acfda300c68cb7d2280cb97fd575bfedac3896e
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2023-12-11 14:14:04 +0600

    slicing done

--- The_GreatShow_of_Python/subsTringUsingSlice.py
+++ The_GreatShow_of_Python/subsTringUsingSlice.py
@@ -1,5 +1,7 @@
 website = "https://google.com" # we need only Google 
-
+website2 = "https://youtube.com" #we need only youtube
 
 slice = slice(8,-4)
-print(website[slice])
+print(website[slice]) # slicing object
+print(website2[slice])
+
